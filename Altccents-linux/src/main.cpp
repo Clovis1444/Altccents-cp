e991276fff18b629767b2e5da134a4a48d9f2ea8
Clovis1444 <eu11111444@gmail.com>
2025-01-19 22:24:29 +0300

    feat(linux): implement key press emulation

--- Altccents-linux/src/main.cpp
+++ Altccents-linux/src/main.cpp
@@ -1,5 +1,7 @@
 #include <fcntl.h>
 #include <libevdev-1.0/libevdev/libevdev.h>
+#include <linux/input-event-codes.h>
+#include <sys/select.h>
 #include <unistd.h>
 
 #include <cerrno>
@@ -11,7 +13,7 @@ int main() {
     int fd{};
     int rc{};
 
-    fd = open("/dev/input/event2", O_RDONLY | O_NONBLOCK);
+    fd = open("/dev/input/event2", O_RDWR | O_NONBLOCK);
 
     rc = libevdev_new_from_fd(fd, &dev);
 
@@ -35,13 +37,47 @@ int main() {
         rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
 
         if (rc == 0 && ev.type == EV_KEY) {
+            if (ev.code == KEY_A) {
+                libevdev_grab(dev, LIBEVDEV_GRAB);
+                continue;
+            }
+            if (ev.code == KEY_S) {
+                libevdev_grab(dev, LIBEVDEV_UNGRAB);
+                continue;
+            }
+            if (ev.code == KEY_Q) {
+                libevdev_grab(dev, LIBEVDEV_UNGRAB);
+                break;
+            }
+            // Emulate keyboard input
+            if (ev.code == KEY_P && ev.value == 1) {
+                input_event virtual_ev{};
+
+                gettimeofday(&virtual_ev.time, nullptr);
+                virtual_ev.type = EV_KEY;
+                virtual_ev.code = KEY_2;
+                virtual_ev.value = 1444;  // 1 key press; 0 - key release
+
+                // Key press
+                write(fd, &virtual_ev, sizeof(virtual_ev));
+                // Key release
+                virtual_ev.value = 0;
+                write(fd, &virtual_ev, sizeof(virtual_ev));
+                // Syncronize events
+                virtual_ev.type = EV_SYN;
+                virtual_ev.code = SYN_REPORT;
+                virtual_ev.value = 0;
+                write(fd, &virtual_ev, sizeof(virtual_ev));
+            }
+
             std::cout << libevdev_event_code_get_name(EV_KEY, ev.code)
-                      << (ev.value ? ((ev.value > 1) ? " REpeated" : " pressed")
+                      << (ev.value ? ((ev.value > 1) ? " REpeated" : "pressed")
                                    : " released")
                       << std::endl;
         }
     }
 
+    libevdev_grab(dev, LIBEVDEV_UNGRAB);
     libevdev_free(dev);
     close(fd);
     return 0;
